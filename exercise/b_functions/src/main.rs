a68ba70b6b7012d2b4374e91e97822ab19c0d911
Raul H. Lopes <raulzc00@gmail.com>
2024-07-02 13:56:43 -0300

    atualizando resposta: exercício b

--- exercise/b_functions/src/main.rs
+++ exercise/b_functions/src/main.rs
@@ -1,12 +1,9 @@
-#![allow(unused_variables)]
-
 fn main() {
     let width = 4;
     let height = 7;
     let depth = 10;
-    let area = area_of(width, height);
 
-    println!("Area is {}", area);
+    println!("Area is {}", area_of(width, height));
 
     println!("Volume is {}", volume(width, height, depth));
 }
